1ce44d71c900ea2fe6e64e892ca1c841fc96e3a4
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2025-05-26 14:53:03 +0200

    More contracts

--- src/contractTest/resources/contracts/people.groovy
+++ src/contractTest/resources/contracts/people.groovy
@@ -1,22 +1,55 @@
 package contracts
 
 import org.springframework.cloud.contract.spec.Contract
+import org.springframework.cloud.contract.spec.internal.MediaTypes
 
-Contract.make {
-    name 'GET people by UID found'
-    request {
-        method 'GET'
-        urlPath('/api/v1/people/john.doe') {
+[
+        Contract.make {
+            name 'GET people by UID found'
+            request {
+                method GET()
+                urlPath('/api/v1/people/john.doe') {
+                }
+                headers {
+                    contentType(MediaTypes.APPLICATION_JSON)
+                }
+            }
+            response {
+                status OK()
+                body file('response.json')
+                headers {
+                    contentType(MediaTypes.APPLICATION_JSON)
+                }
+            }
+        },
+
+        Contract.make {
+            name 'GET people by UID not found'
+            request {
+                method GET()
+                urlPath('/api/v1/people/john.smith') {
+                }
+                headers {
+                    contentType(MediaTypes.APPLICATION_JSON)
+                }
+            }
+            response {
+                status NOT_FOUND()
+            }
+        },
+
+        Contract.make {
+            name 'DELETE people by UID'
+            request {
+                method DELETE()
+                urlPath('/api/v1/people/john.doe') {
+                }
+                headers {
+                    contentType(MediaTypes.APPLICATION_JSON)
+                }
+            }
+            response {
+                status NO_CONTENT()
+            }
         }
-        headers {
-            contentType('application/json')
-        }
-    }
-    response {
-        status OK()
-        body file('response.json')
-        headers {
-            contentType('application/json')
-        }
-    }
-}
+]
