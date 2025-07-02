d958bb3643d5134bb3e5f7b8e9a01fa97ffbdf44
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2025-07-02 17:11:43 +0200

    More monorepo configuration

--- backend-spring/src/main/kotlin/org/lafeuille/demo/domain/PersonResponse.kt
+++ backend-spring/src/main/kotlin/org/lafeuille/demo/domain/PersonResponse.kt
@@ -1,8 +1,10 @@
 package org.lafeuille.demo.domain
 
+import org.springframework.hateoas.server.core.Relation
 import java.time.LocalDate
 
 // @see https://schema.org/Person
+@Relation(itemRelation = "person", collectionRelation = "people")
 data class PersonResponse(
     val identifier: String,
     val name: String,
