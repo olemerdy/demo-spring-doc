1945e3a4620a6fd2d75359e61917c76440982d22
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2025-09-10 09:41:26 +0200

    Changed API

--- backend-spring/src/testFixtures/kotlin/org/lafeuille/demo/domain/PersonFixtures.kt
+++ backend-spring/src/testFixtures/kotlin/org/lafeuille/demo/domain/PersonFixtures.kt
@@ -4,6 +4,18 @@ import java.time.LocalDate
 import java.time.Month
 
 object PersonFixtures {
+    object JaneSmith {
+        const val UID = "jane.smith"
+
+        const val GIVEN_NAME = "Jane"
+
+        const val FAMILY_NAME = "Smith"
+
+        const val FULL_NAME = "$GIVEN_NAME $FAMILY_NAME"
+
+        val BIRTH_DATE: LocalDate by lazy { LocalDate.of(2002, Month.FEBRUARY, 2) }
+    }
+
     object JohnDoe {
         const val UID = "john.doe"
 
@@ -19,12 +31,4 @@ object PersonFixtures {
     object UnknownGuy {
         const val UID = "unknown.guy"
     }
-
-    const val OTHER_GIVEN_NAME = "Jane"
-
-    const val OTHER_FAMILY_NAME = "Smith"
-
-    const val OTHER_FULL_NAME = "$OTHER_GIVEN_NAME $OTHER_FAMILY_NAME"
-
-    val OTHER_BIRTH_DATE: LocalDate by lazy { LocalDate.of(2002, Month.FEBRUARY, 2) }
 }
