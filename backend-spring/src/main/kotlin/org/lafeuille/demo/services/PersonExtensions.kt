9a9dd1e886ef8f2988f58d423ec2ed1f973dabc5
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2025-09-23 16:36:05 +0200

    Factorizing functions

--- backend-spring/src/main/kotlin/org/lafeuille/demo/services/PersonExtensions.kt
+++ backend-spring/src/main/kotlin/org/lafeuille/demo/services/PersonExtensions.kt
@@ -3,12 +3,21 @@ package org.lafeuille.demo.services
 import org.lafeuille.demo.data.PersonEntry
 import org.lafeuille.demo.domain.CountryResponse
 import org.lafeuille.demo.domain.PersonResponse
+import java.time.LocalDate
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
 object PersonExtensions {
     internal val GENERALIZED_TIME_FORMATTER: DateTimeFormatter by lazy { DateTimeFormatter.ofPattern("yyyyMMddHH[mm]VV") }
+
+    fun generalizedTimeToLocalDate(generalizedTime: String): LocalDate =
+        ZonedDateTime.parse(generalizedTime, GENERALIZED_TIME_FORMATTER).toLocalDate()
+
+    fun countryCodeToDisplayCountry(
+        countryCode: String,
+        locale: Locale,
+    ): String = Locale.of("", countryCode).getDisplayCountry(locale)
 }
 
 internal fun PersonEntry.toResponse(locale: Locale) =
@@ -17,10 +26,10 @@ internal fun PersonEntry.toResponse(locale: Locale) =
         name = this.commonName!!,
         familyName = this.surname!!,
         givenName = this.givenName!!,
-        birthDate = ZonedDateTime.parse(this.dateOfBirth!!, PersonExtensions.GENERALIZED_TIME_FORMATTER).toLocalDate(),
+        birthDate = PersonExtensions.generalizedTimeToLocalDate(this.dateOfBirth!!),
         nationality =
             CountryResponse(
                 identifier = this.countryOfCitizenship!!,
-                name = Locale.of("", this.countryOfCitizenship!!).getDisplayCountry(locale),
+                name = PersonExtensions.countryCodeToDisplayCountry(this.countryOfCitizenship!!, locale),
             ),
     )
