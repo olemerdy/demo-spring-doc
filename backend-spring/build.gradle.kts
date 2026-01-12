4e007d389fba24074331f9b18bf9200fdf2f296b
Olivier Le Merdy <olivier.lemerdy+github@gmail.com>
2026-01-12 09:29:58 +0100

    spring-boot-4 (#73)
    * Spring Boot 4
    
    * Spring Boot 4
    
    * Fixing paths
    
    * Bumps
    
    * Rest Assured 6

--- backend-spring/build.gradle.kts
+++ backend-spring/build.gradle.kts
@@ -23,7 +23,7 @@ java {
 dependencyManagement {
     imports {
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:${libs.versions.spring.cloud.asProvider().get()}")
-        mavenBom("org.springdoc:springdoc-openapi-bom:${libs.versions.springdoc.get()}")
+        mavenBom("io.rest-assured:rest-assured-bom:${libs.versions.rest.assured.get()}")
         mavenBom("io.cucumber:cucumber-bom:${libs.versions.cucumber.asProvider().get()}")
     }
 }
@@ -35,13 +35,18 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-ldap")
     implementation("org.springframework.boot:spring-boot-starter-hateoas")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("org.springframework.data:spring-data-rest-hal-explorer")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui")
     testAndDevelopmentOnly("org.springframework.boot:spring-boot-devtools")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-restdocs")
+    testImplementation("org.springframework.boot:spring-boot-starter-actuator-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-ldap-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-hateoas-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-ldap")
+    testImplementation("org.springframework.boot:spring-boot-starter-validation-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
     testImplementation("com.unboundid:unboundid-ldapsdk")
     testImplementation("org.springframework.cloud:spring-cloud-starter-contract-verifier")
     testImplementation("org.springframework.restdocs:spring-restdocs-asciidoctor")
