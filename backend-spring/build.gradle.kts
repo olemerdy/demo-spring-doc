b2ed15ac1444fcf3e93c67ac82f7ebe03427ed8a
Olivier Le Merdy <1182046+olemerdy@users.noreply.github.com>
2026-03-02 14:29:29 +0100

    Versions

--- backend-spring/build.gradle.kts
+++ backend-spring/build.gradle.kts
@@ -75,7 +75,7 @@ spotless {
 
 tasks.withType<KotlinCompile> {
     compilerOptions {
-        freeCompilerArgs.add("-Xjsr305=strict")
+        freeCompilerArgs.addAll("-Xjsr305=strict", "-Xannotation-default-target=param-property")
     }
 }
 
